0863ed31e82a79d653cc14ee323c0a2fe8662c9b
THESV7 <vadhelsmit@gmail.com>
2023-08-10 19:20:59 +0530

    Filter questions add

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,9 +1,35 @@
-const mongoose = require('mongoose')
-const Question = require('../models/questionSchema')
+const mongoose = require("mongoose");
+const Question = require("../models/questionSchema");
 
-const getAllQuestions = async(req,res)=>{
-    const AllData = await Question.find()
-    res.send(AllData)
-}
+const getAllQuestions = async (req, res) => {
+  try {
+    const allData = await Question.find();
+    res.send(allData);
+  } catch (error) {
+    console.error("Error fetching all questions:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-module.exports = getAllQuestions
+const filterQuestions = async (req, res) => {
+  const filterText = req.query.filter;
+
+  try {
+    let query = {};
+
+    if (filterText) {
+      query = { questionText: { $regex: filterText, $options: "i" } };
+    }
+
+    const filteredQuestions = await Question.find(query);
+    res.json(filteredQuestions);
+  } catch (error) {
+    console.error("Error fetching filtered questions:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getAllQuestions,
+  filterQuestions,
+};
--- server/routers/router.js
+++ server/routers/router.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const getAllQuestions = require('../controllers/controller')
-const router = express.Router()
+const express = require("express");
+const {
+  getAllQuestions,
+  filterQuestions,
+} = require("../controllers/controller"); // Import both controller functions
+const router = express.Router();
 
-router.get('/Allquestions' , getAllQuestions)
+router.get("/Allquestions", getAllQuestions);
+router.get("/Filterquestions", filterQuestions); // Add this route for filtering
 
-
-
-module.exports = router
+module.exports = router;
