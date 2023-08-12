edbce874d1f98230315151e2d887373e07268269
sahilwagh03 <sahilwagh142@gmail.com>
2023-08-12 19:27:37 +0530

    category added

--- server/routers/router.js
+++ server/routers/router.js
@@ -2,12 +2,13 @@ const express = require("express");
 const {
   getAllQuestions,
   filterQuestions,
-  getQuestionsLevel
+  getQuestionsLevel,
+  getQuestionCategory
 } = require("../controllers/controller"); // Import both controller functions
 const router = express.Router();
 
 router.get("/Allquestions", getAllQuestions);
 router.get("/Filterquestions", filterQuestions); // Add this route for filtering
 router.get('/Difficulty/:level' , getQuestionsLevel);
-router.get('/Category/:category' , getQuestionsLevel);
+router.get('/Category/:category' , getQuestionCategory);
 module.exports = router;
