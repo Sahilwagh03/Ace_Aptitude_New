2b3f8fca9cb5a7d53f7ff3846f0c5d90990f8029
sahilwagh03 <sahilwagh142@gmail.com>
2023-08-18 23:09:32 +0530

    sort by update

--- src/Components/Aptitude_test/Aptitude_test_page.jsx
+++ src/Components/Aptitude_test/Aptitude_test_page.jsx
@@ -7,15 +7,22 @@ const Aptitude_test_page = () => {
     const navigate = useNavigate()
 
     const [MainData, setMainData] = useState([])
-    const [SelectedCategory , setSelectedCategory] = useState("All")
+    const [SelectedCategory, setSelectedCategory] = useState("All")
 
     const [selectedDifficulty, setSelectedDifficulty] = useState('')
+    const [selectedSort, setSelectedSort] = useState('')
     const [isDifficultOpen, setIsDifficultOpen] = useState(false)
+    const [isSortByOpen, setIsSortByOpen] = useState(false)
     const difficultyoptions = ["Easy", "Medium", "Hard"]
+    const sotrtBy = ["A-Z", "Number"]
 
 
     const handleDropDifficult = () => {
         setIsDifficultOpen(!isDifficultOpen)
+    }
+
+    const handleDropSortBy = () => {
+        setIsSortByOpen(!isSortByOpen)
     }
 
     const handleSelectedOption = (option) => {
@@ -23,25 +30,39 @@ const Aptitude_test_page = () => {
         setIsDifficultOpen(false)
         const lowerCaseOption = option.toLowerCase();
         // difficultyApi(lowerCaseOption)
-        handleCategoriesAndLevel(SelectedCategory,lowerCaseOption)
+        handleCategoriesAndLevel(SelectedCategory, lowerCaseOption)
+    }
+
+    const handleSelectedSort = (option) => {
+        setSelectedSort(option)
+        setIsSortByOpen(false)
+        const lowerCaseSort = option.toLowerCase()
+        if(lowerCaseSort == 'a-z'){
+            const sortedData = MainData.sort((a, b) => a.category.localeCompare(b.category))
+            setMainData(sortedData)
+        }else{
+            const sortedData = MainData.sort((a, b) => parseInt(a._id) - parseInt(b._id))
+            setMainData(sortedData)
+        }
+        console.log(lowerCaseSort)
     }
 
 
     const handleCategoriesAndLevel = async (category, difficulty) => {
         let url = `http://localhost:5000/api/FilterData/`;
-        
+
         if (category) {
             url += category + '/';
         } else {
             url += 'All/';
         }
-    
+
         if (difficulty) {
             url += difficulty;
         } else {
             url += 'All';
         }
-    
+
         try {
             const response = await fetch(url);
             if (!response.ok) {
@@ -52,12 +73,11 @@ const Aptitude_test_page = () => {
             console.log(jsonData);
         } catch (error) {
             console.log(error);
-        } 
+        }
     };
 
 
-    const handleCategory = (category) =>{
-        // categoryApi(category)
+    const handleCategory = (category) => {
         setSelectedCategory(category)
         const lowerCasesDifficulty = selectedDifficulty.toLowerCase();
         handleCategoriesAndLevel(category, lowerCasesDifficulty)
@@ -84,7 +104,7 @@ const Aptitude_test_page = () => {
         getAllcategory()
     }, [])
 
-    const handleQuestionRoute = (category,level)=>{
+    const handleQuestionRoute = (category, level) => {
         navigate(`/aptitude/${category}/${level}`)
     }
 
@@ -111,7 +131,19 @@ const Aptitude_test_page = () => {
                                     }
                                 </div>
                             </div>
-                            <input type="text" placeholder='Sorted by' />
+                            <div className='dropdown'>
+                                <input type="text" placeholder='Sort By' value={selectedSort} onClick={handleDropSortBy} readOnly />
+                                <i className={`bx bx-chevron-down ${isSortByOpen ? 'ArrowRotate' : ''}`} onClick={handleDropSortBy} id="inputArrow"></i>
+                                <div className={`dropdown-content ${isSortByOpen ? 'show' : ''}`} id="difficultyMenu">
+                                    {
+                                        sotrtBy.map((option, index) =>
+                                            <>
+                                                <a key={index} onClick={() => handleSelectedSort(option)}>{option}</a>
+                                            </>
+                                        )
+                                    }
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="aptitude_container">
@@ -119,11 +151,11 @@ const Aptitude_test_page = () => {
                             <div className="category_container">
                                 <h3 className='category_heading'>Category</h3>
                                 <ul className='category_ul'>
-                                    <li className='category_li' onClick={()=> handleCategory("All")}>ALL</li>
-                                    <li className='category_li' onClick={()=> handleCategory("General Aptitude")}>General Aptitude</li>
-                                    <li className='category_li' onClick={()=> handleCategory("Programming")}>Programming</li>
-                                    <li className='category_li' onClick={()=> handleCategory("Verbal")}>Verbal</li>
-                                    <li className='category_li' onClick={()=> handleCategory("Logical Reasoning")}>Logical Reasoning</li>
+                                    <li className='category_li' onClick={() => handleCategory("All")}>ALL</li>
+                                    <li className='category_li' onClick={() => handleCategory("General Aptitude")}>General Aptitude</li>
+                                    <li className='category_li' onClick={() => handleCategory("Programming")}>Programming</li>
+                                    <li className='category_li' onClick={() => handleCategory("Verbal")}>Verbal</li>
+                                    <li className='category_li' onClick={() => handleCategory("Logical Reasoning")}>Logical Reasoning</li>
                                 </ul>
                             </div>
                         </div>
@@ -132,9 +164,9 @@ const Aptitude_test_page = () => {
                             <div className='problem_main'>
                                 {
                                     MainData.map((data, index) =>
-                                        <div onClick={()=> handleQuestionRoute(data.category , data.difficulty)} className="problem_card" key={index}>
+                                        <div onClick={() => handleQuestionRoute(data.category, data.difficulty)} className="problem_card" key={index}>
                                             <div className="problem_category"><span>{data.category}</span></div>
-                                            <div className="problem_level"><span style={data.difficulty=='easy'? {background:'rgb(158 255 158)'} : {background:'#f0f0f0'} && data.difficulty=='medium'? {background:'rgb(253 235 100)'} : {background:'#f0f0f0'} && data.difficulty=="hard" ? {background:'#ff6f6f'}: {background:'#f0f0f0'}}>{data.difficulty}</span></div>
+                                            <div className="problem_level"><span style={data.difficulty == 'easy' ? { background: 'rgb(158 255 158)' } : { background: '#f0f0f0' } && data.difficulty == 'medium' ? { background: 'rgb(253 235 100)' } : { background: '#f0f0f0' } && data.difficulty == "hard" ? { background: '#ff6f6f' } : { background: '#f0f0f0' }}>{data.difficulty}</span></div>
                                         </div>
                                     )
                                 }
