4ec003102bbf3c4f02cdc4d5e1edb0c406526cca
sahilwagh03 <sahilwagh142@gmail.com>
2023-09-08 13:50:26 +0530

    update navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,35 +2,35 @@ import React, { useState } from 'react'
 import './NavBar.css'
 import menu from '../../assets/menu.svg'
 import close from '../../assets/close.svg'
-import { Link , useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import NavLogo from '../../assets/NavLogo.png'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const NavBar = () => {
     const [toggle, setToggle] = useState(false)
     const navigate = useNavigate();
     const variants = {
         initial: {
-          transform: "translateY(-100%)",
-          opacity: 0,
+            transform: "translateY(-100%)",
+            opacity: 0,
         },
         animate: {
-          transform: "translateY(0)",
-          opacity: 1,
-          transition: {
-            duration: 1.5,
-            ease: "easeInOut",
-          },
+            transform: "translateY(0)",
+            opacity: 1,
+            transition: {
+                duration: 1.5,
+                ease: "easeInOut",
+            },
         },
-      };
+    };
 
     return (
         <>
             <motion.div className='Nav_div'
-                  initial="initial"
-                  animate="animate"
-                  variants={variants}
-                  viewport={{ once: true }}
+                initial="initial"
+                animate="animate"
+                variants={variants}
+                viewport={{ once: true }}
             >
                 <nav className='navbar'>
 
@@ -48,8 +48,8 @@ const NavBar = () => {
                         </ul>
                     </div>
                     <div className='btn_div'>
-                        <button className='nav_btn_signup' onClick={()=>navigate('/SignUp')}>Sign up</button>
-                        <button className='nav_btn_login'  onClick={()=>navigate('/Login')}>Login</button>
+                        <button className='nav_btn_signup' onClick={() => navigate('/SignUp')}>Sign up</button>
+                        <button className='nav_btn_login' onClick={() => navigate('/Login')}>Login</button>
                     </div>
 
 
@@ -64,8 +64,8 @@ const NavBar = () => {
                                 <Link to='/about'><li className='nav_text'>About us</li></Link>
                             </ul>
                             <div className='btn_div_mobile'>
-                                <button className='nav_btn_signup' >Sign up</button>
-                                <button className='nav_btn_login'>Login</button>
+                                <button className='nav_btn_signup' onClick={() => navigate('/SignUp')}>Sign up</button>
+                                <button className='nav_btn_login' onClick={() => navigate('/Login')}>Login</button>
                             </div>
                         </div>
 
