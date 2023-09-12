b8b2113f25e31de881b2507e64bccf3c041f8640
sahilwagh03 <sahilwagh142@gmail.com>
2023-09-12 20:04:58 +0530

    api links updated

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
     }
   
     try {
-      const response = await fetch('http://localhost:5000/api/signup', {
+      const response = await fetch('https://ace-aptitude.onrender.com/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
